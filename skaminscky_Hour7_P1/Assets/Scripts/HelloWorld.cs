861efd2df7f2b4dfa74623fcc4a892e0e760ad6d
silly guy <skybusinessemail123@gmail.com>
2024-09-29 19:01:14 -0700

    Hour 7 PT 1

---
+++ skaminscky_Hour7_P1/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print("Hello World!");
+        // or
+        print("Hello" + "World");
+    
+
+        // I was just testing things here dont mind me
+        /*if (<some Boolean condition>)
+        {
+            // Do something
+        }
+        else
+        {
+            // Do something else
+        }
+
+        if (x > y)
+        {
+            print("X is greater than Y");
+        }
+        else
+        {
+            print("Y is greater than X");
+        }
+
+        int sum =0;
+        int count=1;
+
+        while (count ,= 100)
+        {
+            sum+= count;
+            count++;
+        }
+
+        Debug.Log(sum);
+
+        for (<create a counter>; <Boolean conditional>; <increment the counter >)
+        {
+            // Do something
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ skaminscky_Hour7_P1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    public int runSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
