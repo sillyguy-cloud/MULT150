db1d67f178a53ac4c706a5721169e659fb272244
silly guy <skybusinessemail123@gmail.com>
2024-11-10 20:43:45 -0800

    Hour 10

---
+++ SKaminscky_Hour10/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+
+{
+
+    public bool isSolved = false;
+
+    void OnTriggerEnter (Collider collider)
+
+    {
+
+        GameObject collidedWith = collider.gameObject;
+
+        if (collidedWith.tag == gameObject.tag)
+
+        {
+
+            isSolved = true;
+
+            GetComponent<Light>().enabled = false;
+
+            Destroy (collidedWith);
+
+        }
+
+    }
+
+}
